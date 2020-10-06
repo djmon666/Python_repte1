9f0714c17640af17e7c8f5d458f4064a0543637e
Codelearn Banyoles <codelearnbanyoles@gmail.com>
2020-10-06 07:00:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def reverse(phrase):
+  i=""
+  i = i.join(list(map(lambda x: x[::-1], phrase.split())))
+  k=""
+  for si in i:
+    if si=="A":
+      k=k+"T"
+    elif si=="C":
+      k=k+"G"
+    elif si=="G":
+      k=k+"C"
+    else:
+      k=k+"A"
+  return k
+print reverse("ATTGTGCTATCCCTCGACCTTATCAAAGCTTGCTA")
