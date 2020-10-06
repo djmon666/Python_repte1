076c3b9f7b08b97bc49624678e6154f07fa49030
Codelearn Banyoles <codelearnbanyoles@gmail.com>
2020-10-06 07:08:59 +0000

    Pol Amat

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-def reverse(phrase):
-  i=""
-  i = i.join(list(map(lambda x: x[::-1], phrase.split())))
-  k=""
-  for si in i:
-    if si=="A":
-      k=k+"T"
-    elif si=="C":
-      k=k+"G"
-    elif si=="G":
-      k=k+"C"
-    else:
-      k=k+"A"
-  return k
-print reverse("ATTGTGCTATCCCTCGACCTTATCAAAGCTTGCTA")
+def cacabuya(l):
+  o=""
+  i=len(l)-1
+  while i>=0:
+    o+=l[i]
+    i-=1
+  u=""
+  for c in o:
+    if c=="A":
+      u+="T"
+    elif c=="C":
+      u+="G"
+    elif c=="G":
+      u+="C"
+    elif c=="T":
+      u+="A"
+  return u
+
+print cacabuya("ATTGTGCTATCCCTCGACCTTATCAAAGCTTGCTA")
