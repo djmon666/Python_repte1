2cacf82692870bbe7619649932d509acf2540b1b
Codelearn Banyoles <codelearnbanyoles@gmail.com>
2020-10-06 09:58:25 +0000

    Falta replace

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-def reverse(phrase):
-  i=""
-  i = i.join(list(map(lambda x: x[::-1], phrase.split())))
-  k=""
-  for si in i:
-    if si=="A":
-      k=k+"T"
-    elif si=="C":
-      k=k+"G"
-    elif si=="G":
-      k=k+"C"
-    else:
-      k=k+"A"
-  return k
-print(reverse("ATTGTGCTATCCCTCGACCTTATCAAAGCTTGCTA"))
+def caca(c):
+    i=''
+    r=0
+    p=len(c)-1
+    while r<=p:
+        i=i+c[p]
+        p=p-1
+    return i
