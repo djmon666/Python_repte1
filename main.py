23ff26591692a1a918b3016bf4e16162cfafbef2
Codelearn Banyoles <codelearnbanyoles@gmail.com>
2020-10-09 08:13:19 +0000

    Exercici albert

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-def caca(c):
-    i=''
-    r=0
-    p=len(c)-1
-    while r<=p:
-        i=i+c[p]
-        p=p-1
-    return i
+def down(s):
+  x=""
+  result=""
+  y=len(s)
+  while y>0:
+    y-=1
+    x=x+s[y]
+  for i in x:
+    if i=="A":
+      result+="T"
+    if i=="C":
+      result+="G"
+    if i=="G":
+      result+="C"
+    if i=="T":
+      result+="A"
+  return result
