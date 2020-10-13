1c77c27b3bba5eab2b21460ffc7230b53a6ea66d
Codelearn Banyoles <codelearnbanyoles@gmail.com>
2020-10-13 09:19:43 +0000

    Afegits if

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ def caca(c):
     r=0
     p=len(c)-1
     while r<=p:
-        i=i+c[p]
+        if c[p]=="A":
+          i=i+"T"
+        elif  c[p]=="T":
+          i=i+"A"
+        elif  c[p]=="C":
+          i=i+"G"
+        elif  c[p]=="G":
+          i=i+"C"
         p=p-1
     return i
+print caca("ATTGTGCTATCCCTCGACCTTATCAAAGCTTGCTA")
